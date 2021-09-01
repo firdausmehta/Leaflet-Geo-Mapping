9ebcd8dfbfef791aee4ac3665dc2450c090e5d40
Firdaus Mehta <firmehta97@gmail.com>
2021-09-02 00:55:25 +1000

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -57,3 +57,14 @@ legend.onAdd = function () {
     var div = L.DomUtil.create('div', 'info legend'),
         grades = [0, 1, 2, 3, 4, 5],
         labels = [];
+
+// loop through our magnitude intervals and generate a label with a colored square for each interval
+for (var i = 0; i < grades.length; i++) {
+    div.innerHTML +=
+        '<i style="background:' + getColor(grades[i]) + '"></i> ' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+}
+return div;
+};
+legend.addTo(myMap);
+});
