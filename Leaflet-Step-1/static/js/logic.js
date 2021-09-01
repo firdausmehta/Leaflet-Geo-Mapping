771d8677e3c5811663e4a8113e9b0447a1073026
Firdaus Mehta <firmehta97@gmail.com>
2021-09-01 18:42:13 +1000

    Update logic.js

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,13 @@
+// Create map object
+var myMap = L.map("map", {
+	center: [37.09, -95.71],
+	zoom: 5
+});
+  
+// Adding light mode tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+	attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+	maxZoom: 18,
+	id: "light-v10",
+	accessToken: API_KEY
+}).addTo(myMap);
