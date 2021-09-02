d15dc7591f4dfac3da60f7febd3dbb1fba9948fd
Firdaus Mehta <firmehta97@gmail.com>
2021-09-02 19:22:51 +1000

    Update logic.js

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -12,3 +12,17 @@ var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/
   id: "light-v10",
   accessToken: API_KEY
 });
+
+var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "outdoors-v11",
+  accessToken: API_KEY
+});
+
+// Create a baseMaps object
+var baseMaps = {
+  "Satellite": satellite,
+	"Grayscale": grayscale,
+	"Outdoors": outdoors
+};
