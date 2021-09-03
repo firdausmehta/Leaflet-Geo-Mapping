b1c7b3a89883a0a83e21769723d2e1017df0187a
Firdaus Mehta <firmehta97@gmail.com>
2021-09-03 20:28:27 +1000

    Update logic.js

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -132,3 +132,20 @@ circleMarkers.bindPopup("<h3>" + earthFeatures[i].properties.place +
 '<br>' + '[' + coordinates[1] + ', ' + coordinates[0] + ']' + "</p>");
 };
 });
+
+// Legend for the chart
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function () {
+
+	var div = L.DomUtil.create('div', 'info legend'),
+		grades = [0, 1, 2, 3, 4, 5];
+
+	// loop through our magnitude intervals and generate a label with a colored square for each interval
+	for (var i = 0; i < grades.length; i++) {
+		div.innerHTML +=
+			'<i style="background:' + getColor(grades[i]) + '"></i> ' +
+			grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+	}
+	return div;
+};
+legend.addTo(myMap);
